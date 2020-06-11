5fa621314b42fe47d0227684b7e749c7cdc7a5c8
Jada <jada.fagan@gmail.com>
2020-06-11 16:32:00 -0400

    modified script for slides 125-140

--- SQLQueryslides 125-140.sql
+++ SQLQueryslides 125-140.sql
@@ -1,8 +1,21 @@
+--[intro to transactions; scenerio is if a school stops offerin accounting as a major]
+--update major
+	--set Description= 'math'
+	--where code = 'accounting';
+		--[lines 6-8 not finished but correct form. lines 2-4 are sort of the same but not really]
+	--update student 
+	--set majorid= (select id from major where code ='math')
+	--where majorid = 
+--select * from major
+
+
 -- [using insert, update, and delete. slides 127-140]
 insert student 
 	(lastname, firstname, statecode, sat, gpa, majorid)
 	values 
 	 ('Phense', 'Noah', 'GA', '1235', '3.15', (select id from major where code = 'math'));
+	 --[ will show most recent id generated from insert command] 
+		 --select IDENT_CURRENT('student')
 update student
 	set gpa= 3.51, MajorId = (select m.id from major m where m.Description = 'Accounting')
 	where id =62
